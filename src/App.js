7273de39b7e377866520182688738a04fe59522f
Nam <phamhainamkt@gmail.com>
2022-06-10 19:29:34 +0700

    nam

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import logo from './logo.svg';
 import 'antd/dist/antd.css';
 import './App.css';
 // import './assets/css/main-color.css'
-import {Router,Route,Switch} from 'react-router-dom'
-import {createBrowserHistory} from 'history'
+import { Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import HomeTemplate from './templates/HomeTemplate/HomeTemplate';
 import Home from './pages/Home/Home';
 import RoomList from './pages/RoomList/RoomList';
 import RoomDetail from './pages/RoomDetail/RoomDetail';
 import Login from './pages/Login/Login';
-import UserTemplate from './templates/UserTemplate/UserTemplate'
 import Register from './pages/Register/Register';
 import RoomDetailTemplate from './templates/RoomDetailTemplate/RoomDetailTemplate'
 import AdminTemplate from './templates/AdminTemplate/AdminTemplate';
@@ -19,7 +18,6 @@ import Dashboard from './pages/Admin/Dashboard/Dashboard';
 import Booking from './pages/Booking/Booking';
 import Profile from './pages/Profile/Profile';
 
-<<<<<<< HEAD
 import User from './pages/Admin/User/User';
 import AddUser from './pages/Admin/User/AddUser';
 import EditUser from './pages/Admin/User/EditUser';
@@ -40,31 +38,11 @@ import LocationTicket from './pages/Admin/Ticket/VeTheoPhong/LocationTicket';
 import RoomTicket from './pages/Admin/Ticket/VeTheoPhong/RoomTicket';
 import Ticket from './pages/Admin/Ticket/VeTheoPhong/Ticket';
 import RoomAddTicket from './pages/Admin/Ticket/VeTheoPhong/RoomAddTicket';
-=======
-import { Router, Route, Switch, Redirect } from "react-router-dom";
-import { createBrowserHistory } from "history";
-import HomeTemplate from "./templates/HomeTemplate/HomeTemplate";
-import Home from "./pages/Home/Home";
-import RoomList from "./pages/RoomList/RoomList";
-import RoomDetail from "./pages/RoomDetail/RoomDetail";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import RoomDetailTemplate from "./templates/RoomDetailTemplate/RoomDetailTemplate";
-import AdminTemplate from "./templates/AdminTemplate/AdminTemplate";
-import Dashboard from "./pages/Admin/Dashboard/Dashboard";
-import Booking from "./pages/Booking/Booking";
-import Profile from "./pages/Profile/Profile";
-import User from "./pages/Admin/User/User";
-import EditUser from "./pages/Admin/User/EditUser";
->>>>>>> 91273f07495824cdcd3b2e4915a208ce06e7c023
 
 import "antd/dist/antd.css";
 import "./App.css";
-<<<<<<< HEAD
 import { BackTop } from 'antd';
-=======
 import CartTemplate from "./templates/CartTemplate/CartTemplate";
->>>>>>> 91273f07495824cdcd3b2e4915a208ce06e7c023
 
 const style = {
   height: 40,
@@ -83,47 +61,7 @@ function App() {
   return (
     <Router history={history}>
       <Switch>
-<<<<<<< HEAD
-          {/* <HomeTemplate path="/roomdetail" component={RoomDetail}/> */}
-          <HomeTemplate path="/roomdetail/:id" component={RoomDetail}/>
-          <HomeTemplate path="/home" component={Home}/>
-          <HomeTemplate path="/booking" component={Booking}/>
-          <HomeTemplate path="/profile" component={Profile}/>
-          <RoomDetailTemplate path="/roomlist/:id" component={RoomList}/>
-          <RoomDetailTemplate path="/roomlist" component={RoomList}/>
-          <UserTemplate path="/login" component={Login}/>
-          <UserTemplate path="/register" component={Register}/>
-
-          <AdminTemplate path="/admin/user/add" component={AddUser}/>
-          <AdminTemplate path="/admin/user/edit/:id" component={EditUser}/>
-          <AdminTemplate path="/admin/user" component={User}/>
-          
-          <AdminTemplate path="/admin/location/add" component={AddLocation}/>
-          <AdminTemplate path="/admin/location/edit/:id" component={Editlocation}/>
-          <AdminTemplate path="/admin/location" component={Location}/>
-
-          <AdminTemplate path="/admin/room/add/:id" component={AddRoom}/>
-          <AdminTemplate path="/admin/room/edit/:id" component={EditRoom}/>
-          <AdminTemplate path="/admin/room/:id" component={Room}/>
-          
-          <AdminTemplate path="/admin/feedback/add/:id" component={AddFeedback}/>
-          <AdminTemplate path="/admin/feedback/edit/:id" component={EditFeedback}/>
-          <AdminTemplate path="/admin/feedback/:id" component={Feedback}/>
-        
-          <AdminTemplate path="/admin/ticket/location" component={LocationTicket}/>
-          <AdminTemplate path="/admin/ticket/room/:id" component={RoomTicket}/>
-          <AdminTemplate path="/admin/ticket/roomadd/:id" component={RoomAddTicket}/>
-          <AdminTemplate path="/admin/ticket/:id" component={Ticket}/>
-
-          <AdminTemplate path="/admin" component={Dashboard}/>
-
-          
-          <HomeTemplate path="/" component={Home}/>
-=======
-      <Route exact path="/">
-            {type==='ADMIN' ? <Redirect to="admin" /> : <Redirect to="home" />}
-          </Route>
-
+        {/* <HomeTemplate path="/roomdetail" component={RoomDetail}/> */}
         <HomeTemplate path="/roomdetail/:id" component={RoomDetail} />
         <HomeTemplate path="/home" component={Home} />
         <HomeTemplate path="/booking" component={Booking} />
@@ -133,13 +71,32 @@ function App() {
         <Route path="/login" component={Login} />
         <CartTemplate path="/cart/login" component={Login} />
         <Route path="/register" component={Register} />
-        <CartTemplate path="/cart/register" component={Register} />
+
+        <AdminTemplate path="/admin/user/add" component={AddUser} />
         <AdminTemplate path="/admin/user/edit/:id" component={EditUser} />
         <AdminTemplate path="/admin/user" component={User} />
-        <AdminTemplate path="/vitri" component={ViTri} />
+
+        <AdminTemplate path="/admin/location/add" component={AddLocation} />
+        <AdminTemplate path="/admin/location/edit/:id" component={Editlocation} />
+        <AdminTemplate path="/admin/location" component={Location} />
+
+        <AdminTemplate path="/admin/room/add/:id" component={AddRoom} />
+        <AdminTemplate path="/admin/room/edit/:id" component={EditRoom} />
+        <AdminTemplate path="/admin/room/:id" component={Room} />
+
+        <AdminTemplate path="/admin/feedback/add/:id" component={AddFeedback} />
+        <AdminTemplate path="/admin/feedback/edit/:id" component={EditFeedback} />
+        <AdminTemplate path="/admin/feedback/:id" component={Feedback} />
+
+        <AdminTemplate path="/admin/ticket/location" component={LocationTicket} />
+        <AdminTemplate path="/admin/ticket/room/:id" component={RoomTicket} />
+        <AdminTemplate path="/admin/ticket/roomadd/:id" component={RoomAddTicket} />
+        <AdminTemplate path="/admin/ticket/:id" component={Ticket} />
+
         <AdminTemplate path="/admin" component={Dashboard} />
-        {/* <HomeTemplate path="/" component={Home} /> */}
->>>>>>> 91273f07495824cdcd3b2e4915a208ce06e7c023
+
+
+        <HomeTemplate path="/" component={Home} />
       </Switch>
       <BackTop>
         <div style={style}>UP</div>
