2145ac5ac51a33887317f317adaf3f28a837f992
DoVanNhatVN94 <dovannhatdn94@gmail.com>
2022-06-22 22:01:46 +0700

    fix avatar

--- src/redux/Actions/NguoiDungAction.js
+++ src/redux/Actions/NguoiDungAction.js
@@ -1,16 +1,21 @@
+/** @format */
+
 import { history } from "../../App";
 import { quanLyNguoiDung } from "../../services/NguoiDungServices";
-import { LayDSNguoiDungType, layThongTinNguoiDungType,ChiTieTNguoiDungType } from "../Types/NguoiDungType";
+import {
+  LayDSNguoiDungType,
+  layThongTinNguoiDungType,
+  ChiTieTNguoiDungType,
+} from "../Types/NguoiDungType";
 /** @format */
 
 import { message } from "antd";
 
-
 const success = (content) => {
-  message.success(content,3);
+  message.success(content, 3);
 };
 const err = (content) => {
-  message.error(content,5);
+  message.error(content, 5);
 };
 
 //nhat ***
@@ -23,12 +28,10 @@ export const LayDSNguoiDungAction = () => {
           type: LayDSNguoiDungType,
           dsNguoiDung: result.data,
         });
-
       }
     } catch (error) {
       console.log("error", error);
       console.log("error", error.response?.data);
-      
     }
   };
 };
@@ -59,34 +62,49 @@ export const UploadAvaraNDAction = (formData, id) => {
           type: ChiTieTNguoiDungType,
           user: result.data,
         });
-        success('Upload avatar thành công !')
+        success("Upload avatar thành công !");
       }
     } catch (error) {
       console.log("error", error);
       console.log("error", error.response?.data);
-      err("Upload avatar không thành công !")
+      err("Upload avatar không thành công !");
     }
   };
 };
-//*** nhat
+export const capNhatThongTinNguoiDungUserAction = (id, user) => {
+  return async (dispatch) => {
+    try {
+      let result = await quanLyNguoiDung.capNhatThongTinNguoiDung(id, user);
+      if (result.status === 200) {
+        await success("Cập nhập người dùng thành công");
+        await window.location.reload();
+      }
+    } catch (error) {
+      console.log("error", error);
+      console.log("error", error.response?.data);
+      err("Cập nhập người dùng không thành công");
+    }
+  };
+};
 
+//*** nhat
 
 export const themNguoiDungAction = (user) => {
   return async (dispatch) => {
     try {
       let result = await quanLyNguoiDung.themNguoiDung(user);
       if (result.status === 200) {
-        success('Thêm người dùng thành công');
+        success("Thêm người dùng thành công");
         console.log(result.data);
-        history.push('/admin/user')
+        history.push("/admin/user");
       }
     } catch (error) {
       console.log("error", error);
       console.log("error", error.response?.data);
-      err('Thêm người dùng không thành công');
+      err("Thêm người dùng không thành công");
     }
   };
-}
+};
 
 export const layThongTinNguoiDungAction = (id) => {
   return async (dispatch) => {
@@ -103,40 +121,38 @@ export const layThongTinNguoiDungAction = (id) => {
       console.log("error", error);
       console.log("error", error.response?.data);
     }
-  }
-}
+  };
+};
 
 export const capNhatThongTinNguoiDungAction = (id, user) => {
   return async (dispatch) => {
     try {
       let result = await quanLyNguoiDung.capNhatThongTinNguoiDung(id, user);
       if (result.status === 200) {
-        success('Thêm người dùng thành công');
+        success("Thêm người dùng thành công");
         console.log(result.data);
-        history.push('/admin/user')
+        history.push("/admin/user");
       }
     } catch (error) {
       console.log("error", error);
       console.log("error", error.response?.data);
-      err('Thêm người dùng thành công');
+      err("Thêm người dùng thành công");
     }
-  }
-}
+  };
+};
 
 export const xoaNguoiDungAction = (id) => {
   return async (dispatch) => {
     try {
       let result = await quanLyNguoiDung.xoaNguoiDung(id);
       if (result.status === 200) {
-        success('Xóa người dùng thành công');
-        dispatch(LayDSNguoiDungAction())
+        success("Xóa người dùng thành công");
+        dispatch(LayDSNguoiDungAction());
       }
     } catch (error) {
       console.log("error", error);
       console.log("error", error.response?.data);
-      err('Xóa người dùng thành công');
+      err("Xóa người dùng thành công");
     }
-  }
-}
-
-
+  };
+};
